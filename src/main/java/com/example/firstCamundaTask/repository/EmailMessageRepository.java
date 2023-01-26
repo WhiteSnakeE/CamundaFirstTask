e38bbaf0a72c771eca72cf333648b383989238e6
WhiteSnakeE <surtx0119@gmail.com>
2023-01-26 19:22:18 +0200

    changed some packages and added two jira configurations

---
+++ src/main/java/com/example/firstCamundaTask/repository/EmailMessageRepository.java
@@ -0,0 +1,15 @@
+package com.example.firstCamundaTask.repository;
+
+import com.example.firstCamundaTask.model.JiraIssue;
+import org.springframework.stereotype.Repository;
+
+import java.util.Properties;
+
+@Repository
+public interface EmailMessageRepository {
+    String setMessageEmployee(JiraIssue jiraIssue);
+
+    String setMessageBoss(JiraIssue jiraIssue);
+
+
+}
